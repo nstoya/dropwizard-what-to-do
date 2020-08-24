3643adf59f8fde4a7d2dc0c11f1d8d6a28b9ecf8
Nadezhda <nad.stoyanova@gmail.com>
2020-08-24 08:50:25 +0200

    add comments

--- start.sh
+++ start.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env sh
 
+echo "---------------------"
+echo "Building application"
+echo "---------------------"
+
 docker-compose up --build -d
 
+echo "----------------------------------"
+echo "Creating database and example data"
+echo "----------------------------------"
+
 docker-compose exec whattodo-app java -jar dropwizard-what-to-do.jar db migrate config-prod.yml
+
+echo "-------------"
+echo "Shutting down"
+echo "-------------"
 
 docker-compose down
